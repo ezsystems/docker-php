48735a4cf1492000d0a9c43a9f2f76063d295a7e
André R <ar@ez.no>
2016-04-30 14:53:22 +0200

    Build on all master commits

--- bin/.travis/push.sh
+++ bin/.travis/push.sh
@@ -29,12 +29,12 @@ if [ "$1" == "" ]; then
     exit 1
 fi
 if [ "$2" == "" ]; then
-    echo "Argument 2 variable GIT_TAG is not set, format: v1.2.3. Bailing out !"
+    echo "Argument 2 variable FORMAT_VERSION is not set, format: v0 or v1 .. Bailing out !"
     exit 1
 fi
 
 REMOTE_IMAGE="$1"
-GIT_TAG="$2"
+FORMAT_VERSION="$2"
 PHP_VERSION=`docker -l error run ez_php:latest php -r "echo PHP_MAJOR_VERSION.'.'.PHP_MINOR_VERSION;"`
 
 docker images
@@ -46,17 +46,9 @@ docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker tag ez_php:latest "${REMOTE_IMAGE}:${PHP_VERSION}"
 docker tag ez_php:latest-dev "${REMOTE_IMAGE}:${PHP_VERSION}-dev"
 
-# "7.0-v1.2.3"
-docker tag ez_php:latest "${REMOTE_IMAGE}:${PHP_VERSION}-${GIT_TAG}"
-docker tag ez_php:latest-dev "${REMOTE_IMAGE}:${PHP_VERSION}-${GIT_TAG}-dev"
-
-# "7.0-v1.2"
-docker tag ez_php:latest "${REMOTE_IMAGE}:${PHP_VERSION}-${GIT_TAG%.[[:digit:]]}"
-docker tag ez_php:latest-dev "${REMOTE_IMAGE}:${PHP_VERSION}-${GIT_TAG%.[[:digit:]]}-dev"
-
-# "7.0-v1"
-docker tag ez_php:latest "${REMOTE_IMAGE}:${PHP_VERSION}-${GIT_TAG%.[[:digit:]].[[:digit:]]}"
-docker tag ez_php:latest-dev "${REMOTE_IMAGE}:${PHP_VERSION}-${GIT_TAG%.[[:digit:]].[[:digit:]]}-dev"
+# "7.0-v0"
+docker tag ez_php:latest "${REMOTE_IMAGE}:${PHP_VERSION}-${FORMAT_VERSION}"
+docker tag ez_php:latest-dev "${REMOTE_IMAGE}:${PHP_VERSION}-${FORMAT_VERSION}-dev"
 
 # "latest" (optional)
 if [ "$LATEST" == "$PHP_VERSION" ]; then
